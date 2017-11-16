bc5779b085846a300b8a8de2045a1647f7e06ffd
SEVADZ <seva.sattarov@ya.ru>
2017-11-16 22:47:17 +0300

    new

---
+++ bot.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import telebot
+import constant.py
+print("hui")
+bot = telebot.Telebot(constant.token)
---
+++ constant.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+token = "428995919:AAE2xQ7zo1_KgadqMk4bS30iIeHh8PUvzQk"
