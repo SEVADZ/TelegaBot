da8bb2087b079e5114e35bce2d664092ab2a5fd6
SEVADZ <seva.sattarov@ya.ru>
2017-11-17 23:26:18 +0300

    вот это я троль

--- bot.py
+++ bot.py
@@ -5,3 +5,7 @@
 import telebot
 import constant
 bot = telebot.TeleBot(constant.token)
+for i in range(50):
+    bot.send_message(constant.xrom, str(i))
+bot.send_message(constant.xrom, "ХАХА ЗАСПАМИЛ")
+bot.polling(none_stop = True, interval = 0)
--- constant.py
+++ constant.py
@@ -1,2 +1,4 @@
 # -*- coding: utf-8 -*-
 token = "483353912:AAEOKfE5ihcdADO5Uog0CC1hUv-F18ME-KY"
+ya = 185428938
+xrom = 202772998
