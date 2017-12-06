d977c4ebf6315b3d84364808264ae9e31027489b
SEVADZ <seva.sattarov@ya.ru>
2017-12-06 21:42:52 +0300

    gg wp

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import requests
 import telebot
-import constant.py
+import constant
+import os
 
-bot = telebot.telebot(constant.token)
+bot = telebot.TeleBot(constant.token)
+
+#хэндлер скачивания архива с паком в папку
+@bot.message_handler(content_types = ["document"])
+def download_pack(message):
+    pack_info = bot.get_file(message.document.file_id)
+    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(constant.token, pack_info.file_path))
+    path = "C:\\своя игра\\packs\\сырые\\" + message.document.file_name
+    with open(path,'wb') as pack:
+        pack.seek(0)
+        pack.write(file.content)
+        bot.send_message(message.chat.id, "downloaded")
+    path = '""C:\\Program Files\\7-Zip\\7z.exe" x -r "' + path + '" u "-oc:\\своя игра\\packs\\' + message.document.file_name[:len(message.document.file_name) - 4] + '""'
+    os.system(path)
+
+bot.polling()
--- parser.py
+++ parser.py
@@ -1,1 +1,51 @@
 import os
+import xml.etree.ElementTree as etree
+
+#тупа пак раскидывает в массив вида [название раунда, [название темы, [цена вопроса, вопрос, ответ]]]
+def pack(path):
+    tree = etree.parse(path)
+    root = tree.getroot()
+    xmlns = root.tag[:len(root.tag) - 7]
+    x = root.findall(".//" + xmlns + "round")
+    for round in range(len(x)):
+        p = x[round]
+        x[round] = []
+        x[round].append(p.attrib['name'])
+        x[round].append(p.findall(".//" + xmlns + "theme"))
+        for theme in range(len(x[round][1])):
+            p = x[round][1][theme]
+            x[round][1][theme] = []
+            x[round][1][theme].append(p.attrib['name'])
+            x[round][1][theme].append(p.findall(".//" + xmlns + "question"))
+            for question in range(len(x[round][1][theme][1])):
+                p = x[round][1][theme][1][question]
+                x[round][1][theme][1][question] = []
+                x[round][1][theme][1][question].append(p.attrib['price'])
+                x[round][1][theme][1][question].append(p.find(".//" + xmlns + "atom").text)
+                x[round][1][theme][1][question].append(p.find(".//" + xmlns + "answer").text)
+    return x
+
+#тупа по обработаному паку, создает класс, чтобы работать было приятней
+#чтобы создать класс, нужно указать путь на файл content.xml в папке где распакован архив
+class Packislav():
+    def __init__(self, path):
+        self.pack = pack(path)
+        self.rounds_count = self.round_counter() #количество раундов
+    def round_name(self, round): #имя раунда по его номеру
+        return self.pack[round][0]
+    def theme_name(self, round, theme): #имя темы по номеру раунда и номеру темы
+        return self.pack[round][1][theme][0]
+    def question(self, round, theme, question): # массив с вопросом, где 1 - цена, 2 - вопрос, 3 - ответ
+        return self.pack[round][1][theme][1][question]
+    def round_counter(self):
+        return len(self.pack)
+    def theme_counter(self, round): #считает количество тем в раунде, по номеру раунда
+        return len(self.pack[round][1])
+
+x = Packislav("C:\\Users\\Админ\\Desktop\\sml\\sadf\\content.xml")
+for i in range(x.rounds_count):
+    for j in range(x.theme_counter(i)):
+        for n in range(5):
+            print(x.question(i, j, n))
+        print("\n")
+    print("----------------")
